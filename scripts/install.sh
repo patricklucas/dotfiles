90feb823aa1a27fca42d1450c3049820fa3c10ba
Patrick Lucas <me@patricklucas.com>
2013-08-31 20:03:08 -0700

    Don't think I need the pwd check

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,3 @@
-if [ "$PWD" != "$HOME/.dotfiles" ]; then
-    echo >&2 "Must be run from $HOME/.dotfiles"
-    exit 1
-fi
-
 for dotfile in gitconfig vimrc tmux.conf; do
     ln -fs "$PWD/$dotfile" "$HOME/.$dotfile"
 done
